fecf778a79afccd26595828b36040957da952ad9
vishwasrwork <vishwasrwork@gmail.com>
2025-11-20 02:45:36 -0500

    Integrated document ingestion with RAG pipeline
    Changes:
    1. Verified backend to be working fine with
       ingestion, summarization and RAG pipeline.
    
    Signed-off-by: Vishwas R <vishwas.r@ibm.com>

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,5 @@
-import os
-
 OS_HOST = "http://opensearch:9200"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-CHUNK_SIZE = 800
-CHUNK_OVERLAP = 100
-ES_HOST = os.getenv("ES_HOST", "http://opensearch:9300")
+CHUNK_SIZE = 10
+CHUNK_OVERLAP = 2
 MONGO_DB_HOST = "mongodb://mongodb:27017/"
