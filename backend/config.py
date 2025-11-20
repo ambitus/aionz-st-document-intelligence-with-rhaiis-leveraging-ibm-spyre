30caafe3ec2344e5af1fc2231224e2a4b419a856
shreya251170 <shreyaganeshe46@gmail.com>
2025-11-20 00:31:46 -0500

    adding summarization pipeline code for s390x with vLLM endpoint

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,5 @@ OS_HOST = "http://opensearch:9200"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 CHUNK_SIZE = 800
 CHUNK_OVERLAP = 100
-ES_HOST = os.getenv("ES_HOST", "http://localhost:9200")
-MONGO_DB_HOST = "mongodb://localhost:27017/"
+ES_HOST = os.getenv("ES_HOST", "http://opensearch:9300")
+MONGO_DB_HOST = "mongodb://mongodb:27017/"
