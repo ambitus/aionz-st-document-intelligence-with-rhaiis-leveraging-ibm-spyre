fa3cb2fb76aa4346fc7db6d093fa2322ccfd9521
vishwasrwork <vishwasrwork@gmail.com>
2025-12-04 05:44:58 -0500

    Added streaming support for summarization
    Signed-off-by: Vishwas R <vishwas.r@ibm.com>

--- backend/rag.py
+++ backend/rag.py
@@ -2,6 +2,8 @@
 # RAG pipeline
 # ----------------------------
 
+from rhaiis_utils import call_rhaiis_model_without_streaming, call_rhaiis_model
+
 def build_rag_prompt(question: str, chunks):
     context = "\n\n---\n\n".join(doc.page_content for doc in chunks)
 
@@ -18,3 +20,17 @@ def build_rag_prompt(question: str, chunks):
     Answer:
     """
     return prompt.strip()
+
+
+def summarize(doc_content):
+    try:
+        prompt = f""" 
+        Summarize the following document
+        
+        Document:
+        {doc_content}
+        """
+        response = call_rhaiis_model(prompt)
+        return response
+    except:
+        return "Error in document summarization"
