2d3d6b0bc8f34d8107924a810358127b0488ca62
Vishwas R <vishwas.r@ibm.com>
2025-12-16 04:23:53 -0500

    Updated README and config
    Signed-off-by: Vishwas R <vishwas.r@ibm.com>

--- backend/config.py
+++ backend/config.py
@@ -12,8 +12,8 @@ OS_HOST: str = os.getenv("OS_HOST", "http://opensearch:9200")
 EMBEDDING_MODEL: str = os.getenv("EMBEDDING_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
 
 # Text Processing Configuration
-CHUNK_SIZE: int = int(os.getenv("CHUNK_SIZE", "10"))
-CHUNK_OVERLAP: int = int(os.getenv("CHUNK_OVERLAP", "2"))
+CHUNK_SIZE: int = int(os.getenv("CHUNK_SIZE", "25"))
+CHUNK_OVERLAP: int = int(os.getenv("CHUNK_OVERLAP", "5"))
 
 # MongoDB Configuration
 MONGO_DB_HOST: str = os.getenv("MONGO_DB_HOST", "mongodb://mongodb:27017/")
